4eb52c8525234483251b96dc36b8bf50ba2c7002
Job Oboth <trappsavage601@gmail.com>
2025-05-28 09:22:56 +0300

    .

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -2,15 +2,17 @@ document.getElementById("currentyear").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = document.lastModified;
 
 document.getElementById('hamburger').addEventListener('click', function() {
-    var menu = document.getElementById(nav-li);
-    menu.classList.toggle('active')
-})
-
-const hamburger = 
-document.getElementById('hamburger');
-const navli = 
-document.getElementById('nav-li');
-hamburger.addEventListener('click',() => 
-{
-    navLinks.classList.toggle('show');
+    var menu = document.getElementById("nav-menu");
+    menu.classList.toggle('show');
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.getElementById("nav-menu");
+    const navMenu = document.getElementById("nav-menu");
+
+    hamburger.addEventListener("click", function () {
+        navMenu.classList.toggle("active");
+
+        hamburger.classList.toggle("open")
+    })
+})
