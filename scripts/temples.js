5ae7049f6cdd6b8c5523248407de3c5016b93799
Job Oboth <trappsavage601@gmail.com>
2025-05-29 17:30:24 +0300

    .

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,18 +1,27 @@
+// set the current year
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = document.lastModified;
 
+//set the last modified date
 document.getElementById('hamburger').addEventListener('click', function() {
     var menu = document.getElementById("nav-menu");
     menu.classList.toggle('show');
 });
 
+//toggle the nav menu and icon
 document.addEventListener("DOMContentLoaded", function () {
-    const hamburger = document.getElementById("nav-menu");
+    const hamburger = document.getElementById("hamburger");
     const navMenu = document.getElementById("nav-menu");
 
     hamburger.addEventListener("click", function () {
-        navMenu.classList.toggle("active");
+        navMenu.classList.toggle("open");
 
-        hamburger.classList.toggle("open")
-    })
-})
+    // Toggle between hamburger and X symbol
+    if 
+    (navMenu.classList.contains(show)) {
+        hamburger.innerHTML = "&times;"; // X symbol
+    } else {
+        hamburger.innerHTML = "&#9776;"; // hamburger symbol
+    }
+    });
+});
