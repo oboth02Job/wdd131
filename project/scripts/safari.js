7cf9e2729bc631e3e746aa598e1aacb682df3c15
Job Oboth <trappsavage601@gmail.com>
2025-06-14 05:40:39 +0300

    .

--- project/scripts/safari.js
+++ project/scripts/safari.js
@@ -1,3 +1,20 @@
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = document.lastModified;
 
+
+const safariOptions = [
+    { value: "gorilla", text: "3-Day Gorilla Trekking" },
+    { value: "nile", text: "2-Day Nile River Cruise Safari" },
+    { value: "murchison", text: "5-Day Murchison Falls Safari" },
+    { value: "queen", text: "4-Day Queen Elizabeth Safari" },
+    { value: "kidepo", text: "6-day Kidepo Valley National Park Safari" },
+    { value: "sipi", text: "2-Day Sipi Falls Hiking Safari" },
+];
+
+const safariSelect = document.getElementById("safari");
+safariOptions.forEach(option => {
+    const opt = document.createElement("option");
+    opt.value = option.value;
+    opt.textContent = option.text;
+    safariSelect.appendChild(opt);
+});
