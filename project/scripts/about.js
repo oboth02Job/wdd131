11c91c3637e5796f527ea24d08536f9c766eb3d3
Job Oboth <trappsavage601@gmail.com>
2025-06-16 01:31:44 +0300

    .

--- project/scripts/about.js
+++ project/scripts/about.js
@@ -43,3 +43,28 @@ function toggleValues() {
  document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("toggleValuesBtn").addEventListener("click", toggleValues);
  });
+
+document.getElementById("hamburger").addEventListener('click', function() {
+    var menu = document.getElementById("nav-menu");
+    menu.classList.toggle('show');
+});
+
+//toggle the nav menu and the icon
+document.addEventListener("DOMContentLoaded", function () {
+     const hamburger = document.getElementById("hamburger");
+     const navMenu = documentElement.getElementsById("nav-menu");
+
+     hamburger.addEventListener("click", function () {
+        navMenu.classList.toggle("open");
+
+        //Toggle between hamburger and X Symbol
+        if
+        (navMenu.classList.contains(show)) {
+            hamburger.innerHTML = "&times"
+        }
+
+        else {
+            hamburger.innerHTML = "&#9776"
+        }
+     }); 
+});
