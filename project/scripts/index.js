5610a4f67a50b2549a293a7e4b22b3438dce5a2f
Job Oboth <trappsavage601@gmail.com>
2025-06-15 23:28:51 +0300

    .

--- project/scripts/index.js
+++ project/scripts/index.js
@@ -19,5 +19,30 @@ else {
     document.addEventListener("DOMContentLoaded", () => {
         const message = document.getElementById("welcome-message");
         message.textContent = "Welcome back to  Nile Origin Safaris!"
-    })
-}
+    });
+};
+
+document.getElementById("hamburger").addEventListener('click', function() {
+    var menu = document.getElementById("nav-menu");
+    menu.classList.toggle('show');
+});
+
+//toggle the nav menu and the icon
+document.addEventListener("DOMContentLoaded", function () {
+     const hamburger = document.getElementById("hamburger");
+     const navMenu = documentElement.getElementsById("nav-menu");
+
+     hamburger.addEventListener("click", function () {
+        navMenu.classList.toggle("open");
+
+        //Toggle between hamburger and X Symbol
+        if
+        (navMenu.classList.contains(show)) {
+            hamburger.innerHTML = "&times"
+        }
+
+        else {
+            hamburger.innerHTML = "&#9776"
+        }
+     }); 
+});
